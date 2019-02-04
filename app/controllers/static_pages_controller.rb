d09c7d30605c13b7c10c1930970487ed10b7cc6b
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-04 18:42:58 +0300

    adding all the controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def team
+  end
+
+  def contact
+  end
+
+  def welcome
+  end
+
+  def user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/team'
+  get 'static_pages/contact'
+  get 'static_pages/welcome'
+  get 'static_pages/user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
