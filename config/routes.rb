567ebcddc1bdd1a90221239667172301b5bb473e
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-04 19:11:23 +0300

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages/home'
   get 'static_pages/home'
-  get 'static_pages/team', as: 'team'
-  get 'static_pages/contact', as: 'contact'
-  get 'static_pages/welcome', as: 'welcome/'
-  get 'static_pages/user', as: 'user'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/team', to: 'static_pages#team', as: 'team'
+  get 'static_pages/contact', to: 'static_pages#contact', as: 'contact'
+  get 'static_pages/welcome', to: "static_pages#welcome", as: 'welcome'
+  get 'static_pages/user', to: "static_pages#user", as: 'user'
 end
