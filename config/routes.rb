5377e4817454b906a1119e9448c2455b6dc3fb12
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-04 19:07:21 +0300

    adding some modification

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages/home'
   get 'static_pages/home'
-  get 'static_pages/team'
-  get 'static_pages/contact'
-  get 'static_pages/welcome'
-  get 'static_pages/user'
+  get 'static_pages/team', as: 'team'
+  get 'static_pages/contact', as: 'contact'
+  get 'static_pages/welcome', as: 'welcome/'
+  get 'static_pages/user', as: 'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
