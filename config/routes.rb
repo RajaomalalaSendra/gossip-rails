53110eb90a0cd6432945ee984638a0c987bb819a
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-05 10:47:31 +0300

    modification of routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'gossips/index'
-  get 'gossips/show'
-  get 'gossips/new'
-  get 'gossips/create'
-  get 'gossips/edit'
-  get 'gossips/update'
-  get 'gossips/destroy'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/team', to: 'static_pages#team'
   get 'static_pages/contact', to: 'static_pages#contact'
   get 'welcome/:id', to: "welcome#welcome"
-   get 'user/:id', to: "user#user"
+  get 'user/:id', to: "user#user"
   get 'static_pages/user/:id', to: "static_pages#user"
+  get '/gossips', to: 'gossips#index'
+  get '/gossips/:id', to: 'gossips#show'
+  get '/gossips/new', to: 'gossips#new'
+  post '/gossips', to: 'gossips#create'
+  get '/gossips/:id/edit', to: 'gossips#edit'
+  post '/gossips/:id', to: 'gossips#update'
+  delete '/gossips/:id', to: 'gossips#destroy'
 end
