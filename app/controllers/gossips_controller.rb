ddf9c4c898dde89e34d437ec16e1a71894ec36c7
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-05 15:02:41 +0300

    some modification like gossip controller

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,15 +1,23 @@
 class GossipsController < ApplicationController
   def index
+    @users = User.all
+    @gossips = Gossip.all
   end
 
   def show
   end
 
   def new
-    puts params
+    puts "#{params}"
   end
 
   def create
+    @gossip = Gossip.new(title: "#{params["get_title"]}", content: "#{params["get_content"]}", user_id: rands(1..@user.length) )
+    if @gossip.save
+      redirect "/"
+    else
+      redirect "/gossips/new"
+    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-resources 'gossips' do
-  resources :comments
-end
-  root 'static_pages#home'
-  get 'static_pages/home'
+  resources 'gossips'
+  root 'gossips#index'
   get 'static_pages/team', to: 'static_pages#team'
   get 'static_pages/contact', to: 'static_pages#contact'
   get 'welcome/:id', to: "welcome#welcome"
   get 'user/:id', to: "user#user"
   get 'static_pages/user/:id', to: "static_pages#user"
-  # get '/gossips', to: 'gossips#index'
-  # get '/gossips/:id', to: 'gossips#show'
-  # get '/gossips/new', to: 'gossips#new'
-  # # post '/gossips', to: 'gossips#create'
-  # get '/gossips/:id/edit', to: 'gossips#edit'
-  # post '/gossips/:id', to: 'gossips#update'
-  # delete '/gossips/:id', to: 'gossips#destroy'
 end
